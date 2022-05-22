4710476b7920975e04b88d0e784fde2bcec8932e
Yohan González <yohan.gonzalez.almaguer@gmail.com>
2022-05-21 21:56:24 -0400

    Updated all the interfaces as detailed in the Rest API

--- src/utils/interfaces/invoice.ts
+++ src/utils/interfaces/invoice.ts
@@ -2,11 +2,17 @@ export interface Invoice {
     number: string,
     asset: string,
     blockchain: string,
+    network: string,
     status: string,
     amount: string,
     description: string,
-    hidden_message: string,
     metadata: string,
+    checkout_url: string,
+    onchain_url: string,
+    success_url: string,
+    cancel_url: string,
+    explorer_url: string,
+    expires_at: string,
     created_at: string,
     updated_at: string
 }
